727e8491d994e6bc4017c25aa6f88f91b9a64d7a
James Watt <james.watt529@outlook.com>
2022-06-11 21:38:52 +0100

    Create GunControl component and use it in index

---
+++ components/GunPolicy.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="policy">
+      <h2 class="policy-header">
+        {{ name }}
+      </h2>
+
+      <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+  props: {
+    name: String
+  },
+  setup(props) {
+    console.log(props.name)
+  }
+}
+</script>
+
+
+<style scoped>
+  .policy-header {
+    font-family: 'Tiro Gurmukhi', serif;
+    margin-bottom: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 30px;
+    border-bottom: 2px solid black;
+    width: fit-content;
+  }
+
+  .policy {
+    text-align: center;
+    margin-bottom: 100px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,43 @@
 <template>
   <div>
-    <div class="policy">
-      <h2 class="policy-header">1. Gun Control Policy One</h2>
+    <GunPolicy name="1. Gun Control Policy One">
       <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
+    </GunPolicy>
 
-    <div class="policy">
-      <h2 class="policy-header">2. Gun Control Policy One</h2>
+    <GunPolicy name="1. Gun Control Policy One">
       <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
+    </GunPolicy>
 
-    <div class="policy">
-      <h2 class="policy-header">3. Gun Control Policy One</h2>
+    <GunPolicy name="1. Gun Control Policy One">
       <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
+
+    <GunPolicy name="1. Gun Control Policy One">
+      <p>m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </GunPolicy>
   </div>
 </template>
-
-<script>
-
-
-</script>
-
-<style>
-
-  .policy-header {
-    font-family: 'Tiro Gurmukhi', serif;
-    margin-bottom: 15px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 0 30px;
-    border-bottom: 2px solid black;
-    width: fit-content;
-  }
-
-  .policy {
-    text-align: center;
-    margin-bottom: 100px;
-  }
-</style>
