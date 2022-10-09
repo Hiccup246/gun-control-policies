3f8c6c500adda99fc19205453f7e2e8adb47c5b1
James Watt <james.watt529@outlook.com>
2022-10-09 18:19:44 +0100

    Update created by box from div to button

--- components/GunControlFooter.vue
+++ components/GunControlFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="footer">
     <james-watt-calling-card modal-bg-color="white">
-      <div class="footer__created-by-wrapper">
-        <p class="footer__created-by">
+      <button class="footer__created-by-button">
+        <p class="footer__created-by-text">
           Created by James Watt
         </p>
-      </div>
+      </button>
     </james-watt-calling-card>
   </div>
 </template>
@@ -20,14 +20,15 @@
     justify-content: center;
   }
 
-  .footer__created-by-wrapper {
+  .footer__created-by-button {
     background-color: white;
     width: fit-content;
     margin: 0 auto;
+    padding: 0;
     border: 3px solid white;
   }
 
-  .footer__created-by {
+  .footer__created-by-text {
     margin: 0;
     font-size: small;
     width: 100%;
@@ -38,7 +39,11 @@
     font-weight: bold;
   }
 
-  .footer__created-by-wrapper:hover > .footer__created-by {
+  .footer__created-by-button:hover > .footer__created-by-text {
     opacity: 0.7;
+  }
+
+  james-watt-calling-card {
+    margin: 0 auto;
   }
 </style>
