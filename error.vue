bad236cca03daeba61719886be81b603e8a16bd7
James Watt <james.watt529@outlook.com>
2026-06-28 17:31:24 +0100

    Fix eslint prop earnings on error.vue

--- error.vue
+++ error.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import type { NuxtError } from "#app";
-
-const _props = defineProps({
-  error: Object as () => NuxtError
-});
+  const props = defineProps<{ error: NuxtError }>();
 </script>
 
 <template>
   <div>
-    <h1>{{ error?.statusCode }}</h1>
+    <h1>{{ props.error.statusCode }}</h1>
+
     <NuxtLink to="/">
       Go back home
     </NuxtLink>
