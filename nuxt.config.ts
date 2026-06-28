6fc1775b05abf3ed0c53b49249a4e7cc43be033b
James Watt <james.watt529@outlook.com>
2026-06-28 17:27:44 +0100

    Add nuxt based linting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-10-19",
+
   runtimeConfig: {
     // Keys within public are also exposed client-side
     public: {
@@ -8,12 +9,16 @@ export default defineNuxtConfig({
       UMAMI_WEBSITE_URL: process.env.UMAMI_WEBSITE_URL
     }
   },
+
   css: [
     "@/assets/css/main.css"
   ],
+
   vue: {
     compilerOptions: {
       isCustomElement: tag => ["james-watt-calling-card"].includes(tag)
     }
-  }
+  },
+
+  modules: ["@nuxt/eslint"]
 });
